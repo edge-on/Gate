5dd093bc5341c7dcfee1fec0f69d6a00077ecf09
wyrexdev <uchihasasukedev@gmail.com>
2026-02-17 22:33:40 +0300

    Add connection handling to Atlas class and implement socket communication
    - Added necessary includes for socket programming and SSL.
    - Implemented socket creation and connection logic in the Atlas constructor.
    - Introduced methods for connecting to Atlas and sending data.
    - Updated private members to manage socket and SSL context.

--- include/Atlas/Atlas.hpp
+++ include/Atlas/Atlas.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <openssl/ssl.h>
+#include <unordered_map>
+
 #include "Cassandra/Cassandra.hpp"
 
 class Atlas
@@ -8,6 +22,16 @@ public:
     Atlas();
     ~Atlas();
 
+    bool connectToAtlas();
+    
+    std::string sendToAtlas();
+
 private:
     Cassandra* cas;
+
+    int client_fd;
+
+    int PORT;
+    
+    SSL_CTX* ctx;
 };
