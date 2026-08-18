7c7b964b99d141c1cf8e1fd5975c3693e1c1d0a0
wyrexdev <uchihasasukedev@gmail.com>
2026-08-18 07:49:37 +0300

    fix(VNStat): improve traffic data retrieval with safe access checks

--- src/Helper/VNStat.cpp
+++ src/Helper/VNStat.cpp
@@ -8,8 +8,10 @@ uint64_t Helper::VNStat::getDailyTraffic()
     std::string vnstat = Helper::Process::execCommand(cmd.data());
     auto json = nlohmann::json::parse(vnstat);
 
-    uint64_t rx = json["interfaces"][0]["traffic"]["day"][0]["rx"];
-    uint64_t tx = json["interfaces"][0]["traffic"]["day"][0]["tx"];
+    const auto &day_node = json["interfaces"][0]["traffic"]["day"][0];
+
+    uint64_t rx = (day_node.contains("rx") && day_node["rx"].is_number()) ? day_node["rx"].get<uint64_t>() : 0;
+    uint64_t tx = (day_node.contains("tx") && day_node["tx"].is_number()) ? day_node["tx"].get<uint64_t>() : 0;
 
     return rx + tx;
 }
